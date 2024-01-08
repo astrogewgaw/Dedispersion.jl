a7dd192efb88792de75c50ea6dbe0b9973fe574b
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-01-08 16:11:08 +0530

    ✨ Turn into a meta package.

--- src/Dedispersion.jl
+++ src/Dedispersion.jl
@@ -1,2 +1,9 @@
 module Dedispersion
+
+using Reexport
+
+@reexport using FastDedispersion: fastdd
+@reexport using DirectDedispersion: dd!, dd
+@reexport using RollingDedispersion: rolldd!, rolldd
+
 end
